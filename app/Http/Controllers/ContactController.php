b0eed2eb536b44f1cec4ff1831cdc835e7269588
Bruce Olivier <bolivierjr@gmail.com>
2018-01-12 07:47:40 -0600

    Added a create view to add contacts and some form validation.

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
@@ -44,7 +45,27 @@ class ContactController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Checks to see if address, city, state, zip field is populated. If so, then require all.
+        if ($request->address || $request->city || $request->state || $request->zipcode) {
+            Validator::make($request->all(), [
+                'firstname' => 'required|string|max:64',
+                'lastname' => 'required|string|max:64',
+                'email' => 'sometimes|nullable|email|max:64',
+                'phone' => 'sometimes|nullable|max:20',
+                'address' => 'required|string|max:64',
+                'city' => 'required|string|max:32',
+                'state' => 'required|string|size:2|regex:/([a-zA-Z])$/',
+                'zipcode' => 'required|string|max:10|regex:/^[0-9]{5}(\-[0-9]{4})?$/',
+            ])->validate();
+        } else {  // If not, then do not require those fields
+            Validator::make($request->all(), [
+                'firstname' => 'required|string|max:64|regex:/[a-zA-Z]/',
+                'lastname' => 'required|string|max:64',
+                'email' => 'sometimes|nullable|email|max:64',
+                'phone' => 'sometimes|nullable|max:20',
+            ])->validate();
+        }
+
     }
 
     /**
--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,5 +1,128 @@
-@extends('layouts.app)
+@extends('layouts.app')
 
 @section('content')
+  <form class="form-horizontal" method="POST" action="{{ route('contacts.store') }}">
+    {{ csrf_field() }}
 
+    <div class="form-group{{ $errors->has('firstname') ? ' has-error' : '' }}">
+      <label for="firstname" class="col-md-4 control-label">First Name</label>
+
+      <div class="col-md-6">
+        <input id="firstname" type="text" class="form-control" name="firstname" value="{{ old('firstname') }}" autofocus>
+
+        @if ($errors->has('firstname'))
+          <span class="help-block">
+            <strong>{{ $errors->first('firstname') }}</strong>
+          </span>
+        @endif
+      </div>
+    </div>
+
+    <div class="form-group{{ $errors->has('lastname') ? ' has-error' : '' }}">
+      <label for="lastname" class="col-md-4 control-label">Last Name</label>
+
+      <div class="col-md-6">
+        <input id="lastname" type="text" class="form-control" name="lastname" value="{{ old('lastname') }}">
+
+        @if ($errors->has('lastname'))
+          <span class="help-block">
+            <strong>{{ $errors->first('lastname') }}</strong>
+          </span>
+        @endif
+      </div>
+    </div>
+
+    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+      <label for="email" class="col-md-4 control-label">Email</label>
+
+      <div class="col-md-6">
+        <input id="email" type="text" class="form-control" name="email" value="{{ old('email') }}">
+
+        @if ($errors->has('email'))
+          <span class="help-block">
+            <strong>{{ $errors->first('email') }}</strong>
+          </span>
+        @endif
+      </div>
+    </div>
+
+    <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
+      <label for="phone" class="col-md-4 control-label">Phone #</label>
+
+      <div class="col-md-6">
+        <input id="phone" type="text" class="form-control" name="phone" value="{{ old('phone') }}">
+
+        @if ($errors->has('phone'))
+          <span class="help-block">
+            <strong>{{ $errors->first('phone') }}</strong>
+          </span>
+        @endif
+      </div>
+    </div>
+
+    <div class="form-group{{ $errors->has('address') ? ' has-error' : '' }}">
+      <label for="address" class="col-md-4 control-label">Street Address</label>
+
+      <div class="col-md-6">
+        <input id="address" type="text" class="form-control" name="address" value="{{ old('address') }}">
+
+        @if ($errors->has('address'))
+          <span class="help-block">
+            <strong>{{ $errors->first('address') }}</strong>
+          </span>
+        @endif
+      </div>
+    </div>
+
+    <div class="form-group{{ $errors->has('city') ? ' has-error' : '' }}">
+      <label for="city" class="col-md-4 control-label">City</label>
+
+      <div class="col-md-6">
+        <input id="city" type="text" class="form-control" name="city" value="{{ old('city') }}">
+
+        @if ($errors->has('city'))
+          <span class="help-block">
+            <strong>{{ $errors->first('city') }}</strong>
+          </span>
+        @endif
+      </div>
+    </div>
+
+    <div class="form-group{{ $errors->has('state') ? ' has-error' : '' }}">
+      <label for="state" class="col-md-4 control-label">State</label>
+
+      <div class="col-md-6">
+        <input id="state" type="text" class="form-control" name="state" value="{{ old('state') }}">
+
+        @if ($errors->has('state'))
+          <span class="help-block">
+            <strong>{{ $errors->first('state') }}</strong>
+          </span>
+        @endif
+      </div>
+    </div>
+
+    <div class="form-group{{ $errors->has('zipcode') ? ' has-error' : '' }}">
+      <label for="zipcode" class="col-md-4 control-label">Zipcode</label>
+
+      <div class="col-md-6">
+        <input id="zipcode" type="text" class="form-control" name="zipcode" value="{{ old('zipcode') }}">
+
+        @if ($errors->has('zipcode'))
+          <span class="help-block">
+            <strong>{{ $errors->first('zipcode') }}</strong>
+          </span>
+        @endif
+      </div>
+    </div>
+
+    <div class="form-group">
+      <div class="col-md-8 col-md-offset-4">
+        <button type="submit" class="btn btn-primary">
+          Save
+        </button>
+      </div>
+    </div>
+
+  </form>
 @endsection
