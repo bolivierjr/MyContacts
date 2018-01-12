f4e43205cbeb1acf5233f06b1e89a67335187ab1
vagrant <vagrant@homestead>
2018-01-12 02:48:26 +0000

    Added resource routes, add contact button, and migrated People table.

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -5,10 +5,17 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Contacts</div>
+
+                <div class="panel-heading">
+                    <strong>Contacts</strong>
+                    <div class="text-center pull-right">
+                        <a href="{{ route('contacts.create') }}" class="btn btn-success btn-sm">Add Contact</a>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
 
                 <div class="panel-body">
-                    You are logged in!
+
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/contacts', 'ContactController@index')->name('contacts');
+Route::resource('contacts', 'ContactController');
